a3dc5cc04425304ce3a1eeb037d03873ee8d7006
Sofia Paiva <80056890+sofia285@users.noreply.github.com>
2022-12-02 11:57:16 +0000

    Add files via upload

---
+++ proj1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+    int n, m, i, j, aux, zeros = 0;
+    /*if (scanf("%d", &n) != 1) {
+        fprintf( stderr, "Expected at least two numbers as input\n");
+        exit(-1);
+    }
+    if (scanf("%d", &m) != 1) {
+        fprintf( stderr, "Expected at least two numbers as input\n");
+        exit(-1);
+    }*/
+
+    cin >> n;
+    cin >> m;
+
+    int tiles[n][m] = {{0}};
+
+    for (i = 1; i <= n; i++){
+        for(j = 1; j <= m; j++) {
+            tiles[i][j] = 0;
+        }
+    }
+
+
+    // mete a um os pontos recebidos
+    for (i = 1; i <= n; i++) {
+        cin >> aux;
+        if (aux > 0) {
+            tiles[i][aux] = 1;
+        }
+        if (aux == 0) {
+            zeros++;
+        }
+        
+    }
+
+    // preenche a area toda a ser ladrilhada
+    for (i = zeros + 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            if (tiles[i][j] == 1) {
+                break;
+            }
+
+            else {
+                tiles[i][j] = 1;
+            }
+        }
+    }
+
+  return 0;
+}
