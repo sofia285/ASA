c9f20108f5a7d0fa47ef82dda1a468d6e9fe872c
Sofia Paiva <2003sasp@gmail.com>
2022-12-06 10:54:22 +0000

    nothing

--- proj1.cpp
+++ proj1.cpp
@@ -38,45 +38,6 @@ int main () {
         }
         cout << endl;
     }
-    /*
-    int n, m, i, j, aux, zeros = 0;
-
-    cin >> n;
-    cin >> m;
-
-    int tiles[n][m] = {{0}};
-
-    for (i = 1; i <= n; i++){
-        for(j = 1; j <= m; j++) {
-            tiles[i][j] = 0;
-        }
-    }
-
-
-    // mete a um os pontos recebidos
-    for (i = 1; i <= n; i++) {
-        cin >> aux;
-        if (aux > 0) {
-            tiles[i][aux] = 1;
-        }
-        if (aux == 0) {
-            zeros++;
-        }
-        
-    }
-
-    // preenche a area toda a ser ladrilhada
-    for (i = zeros + 1; i <= n; i++) {
-        for (j = 1; j <= m; j++) {
-            if (tiles[i][j] == 1) {
-                break;
-            }
-
-            else {
-                tiles[i][j] = 1;
-            }
-        }
-    }*/
 
   return 0;
 }
