bf20d3fc5cf880b9fb39ee65a22b63cbf7a7766f
Sofia Paiva <2003sasp@gmail.com>
2022-12-07 22:33:46 +0000

    matriz com valores dos quadrados

--- proj1.cpp
+++ proj1.cpp
@@ -13,6 +13,7 @@ int main () {
     cin >> m;
     vector<vector<int>> tiles = vector<vector<int>>(n, vector<int>(m, 0));
 
+    // colocação dos 1 na matriz
     for(i = 0; i < n; i++) {
         cin >> aux;
         if (aux > 0) {
@@ -24,6 +25,18 @@ int main () {
             tiles.at(i).at(aux) = 0;
     }
 
+    for (i = 1; i < n; i++) {
+        for (j = m - 2; j >= 0; j--) {
+            if (tiles.at(i).at(j) != 0) {
+                tiles.at(i).at(j) = tiles.at(i - 1).at(j + 1) + 1;
+            }
+        }
+    }
+
+    
+
+
+    // print da matriz com 0 e 1
     for(i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
             cout << tiles[i][j] << " ";
