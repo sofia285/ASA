3638286269858280c7a361262273e6850dd3dbb7
Sofia Paiva <2003sasp@gmail.com>
2022-12-31 13:01:58 +0000

    small changes

--- proj2.cpp
+++ proj2.cpp
@@ -1,34 +1,83 @@
+#include <algorithm>
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
-void printList(int E, vector<vector<int>> list) {
+struct Edge {
+  int start;
+  int end;
+  int weight;
+};
 
-    for (int i = 1; i <= E; i++) {
-        for (int j = 1; j <= E; j++) {
-            cout << list.at(i).at(j) << " ";
-        }
-        cout << endl;
+// Guarda as ligações entre vértices
+class Union {
+ public:
+  vector<int> parent;
+
+  Union(int n) {
+    parent.resize(n);
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
     }
-}
+  }
+
+  int find(int x) {
+    if (x != parent[x]) {
+      parent[x] = find(parent[x]);
+    }
+    return parent[x];
+  }
+
+  void unite(int x, int y) {
+    int root_x = find(x);
+    int root_y = find(y);
+    if (root_x != root_y) {
+      parent[root_x] = root_y;
+    }
+  }
+};
 
 int main() {
-    int V, E, i, W, u, v;
+  int V, E, u, v, w;
+  cin >> V >> E;  
 
-    cin >> V;
-    cin >> E;
-    vector<vector<int>> tiles = vector<vector<int>>(E + 1, vector<int>(E + 1, 0));
-    for (i = 0; i < E; i++) {
-        cin >> u;
-        cin >> v;
-        cin >> W;
-        tiles.at(u).at(v) = W;
-    }
+  vector<Edge> edges;  // Vector to store the edges of the graph
 
-    printList(E, tiles);
+  // Read the edges from the input
+  for (int i = 0; i < E; i++) {
+    cin >> u >> v >> w;
+    Edge e;
+    e.start = u;
+    e.end = v;
+    e.weight = w;
+    edges.push_back(e);
+  }
 
-    return 0;
+  // Sort the edges by weight
+  sort(edges.begin(), edges.end(),
+       [](const Edge &a, const Edge &b) { return a.weight > b.weight; });
+
+
+  Union uf(V);
+
+  int sol = 0;
+
+  for (const Edge &e : edges) {
+    // If adding the edge would create a cycle in the minimum spanning tree, discard the edge
+    if (uf.find(e.start) != uf.find(e.end)) {
+      
+      sol += e.weight;
+      // Merge the connected components of the start and end vertices
+      uf.unite(e.start, e.end);
+
+      if (uf.parent.size() == 1) {
+        break;
+      }
+    }  
+  }
+
+  cout << sol << endl;
+
+  return 0;
 }
