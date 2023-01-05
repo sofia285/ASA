5a1f5b853f881dc3e77e24b00d60e1732654b726
Mariana <mari.miranda2224@gmail.com>
2023-01-05 09:15:24 +0000

    13

--- proj2.cpp
+++ proj2.cpp
@@ -14,12 +14,15 @@ struct Edge {
 class UnionFind {
  public:
   vector<int> parent;
+  vector<int> rank;
 
   UnionFind(int n) {
     parent.resize(n);
+    rank.resize(n);
     // Inicializa o vetor de pais com os próprios vértices
     for (int i = 0; i < n; i++) {
       parent[i] = i;
+      rank[i] = 0; 
     }
   }
 
@@ -35,20 +38,28 @@ class UnionFind {
     int root_x = find(x);
     int root_y = find(y);
     if (root_x != root_y) {
-      parent[root_y] = root_x;
+      if (rank[root_x] < rank[root_y]) {
+        parent[root_x] = root_y;
+      } else if (rank[root_x] > rank[root_y]) {
+        parent[root_y] = root_x;
+      } else {  // rank[root_x] == rank[root_y]
+        parent[root_y] = root_x;
+        rank[root_x]++;
+      }
     }
   }
 };
 
 int main() {
   int V, E, u, v, w;
-  cin >> V >> E;  
+  // cin >> V >> E; 
+  scanf("%d %d", &V, &E); 
 
   vector<Edge> edges;  // Vector to store the edges of the graph
 
   // Read the edges from the input
   for (int i = 0; i < E; i++) {
-    cin >> u >> v >> w;
+    scanf("%d %d %d", &u, &v, &w);
     Edge e;
     e.start = u;
     e.end = v;
@@ -64,7 +75,6 @@ int main() {
   UnionFind uf(V);
 
   int sol = 0;
-
   for (const Edge &e : edges) {
     // If adding the edge would create a cycle in the minimum spanning tree, discard the edge
     if (uf.find(e.start) != uf.find(e.end)) {
@@ -73,13 +83,10 @@ int main() {
       // Merge the connected components of the start and end vertices
       uf.unite(e.start, e.end);
 
-      if (uf.parent.size() == 1) {
-        break;
-      }
     }  
   }
 
-  cout << sol << endl;
+  printf("%d\n", sol);
 
   return 0;
 }
